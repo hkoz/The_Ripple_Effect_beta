5fa96a2927463a38ddb358d984da8fbd11f0ace3
HKOz Honey <hkoz.pi@gmail.com>
2019-09-27 02:27:01 +0300

    Added the ability to sign up

---
+++ app/src/main/java/com/therippleeffect/LogInActivity.kt
@@ -0,0 +1,69 @@
+package com.therippleeffect
+
+import android.annotation.SuppressLint
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+
+class LogInActivity : AppCompatActivity() {
+    val mauth = FirebaseAuth.getInstance()
+    var emailEditText: EditText? = null
+    var passwordEditText: EditText? = null
+    var logInTrue: Boolean = true
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_log_in)
+        emailEditText = findViewById(R.id.email_edit_text)
+        passwordEditText = findViewById(R.id.password_edit_text)
+        logInTrue = true
+
+    }
+    fun logIn(view: View) {
+        var email = emailEditText?.text.toString()
+        var password = passwordEditText?.text.toString()
+        if (emailEditText!= null && passwordEditText!=null && email != "" && password != ""){
+                mauth.signInWithEmailAndPassword(email, password).addOnCompleteListener(this) { Task ->
+                    if (Task.isSuccessful){
+                        logInTrue = true
+                        enterAsMember()
+
+                    }
+                    else{ Toast.makeText(this, getString(R.string.failed_log_in), Toast.LENGTH_SHORT).show() }
+                }
+            }
+            else
+            { Toast.makeText(this, getString(R.string.failed_log_in), Toast.LENGTH_SHORT).show() }
+
+    }
+    fun signup(view: View) {
+        var email = emailEditText?.text.toString()
+        var password = passwordEditText?.text.toString()
+        if (emailEditText!= null && passwordEditText!=null && email != "" && password != ""){
+            mauth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(this) { Task ->
+                if (Task.isSuccessful){
+                    logInTrue = false
+                    enterAsMember()
+
+                }
+                else{ Toast.makeText(this, getString(R.string.failed_sign_up), Toast.LENGTH_SHORT).show() }
+            }
+        }
+        else
+        { Toast.makeText(this, getString(R.string.failed_sign_up), Toast.LENGTH_SHORT).show() }
+
+    }
+    private fun enterAsMember() {
+        if (logInTrue) {
+            Toast.makeText(this, getString(R.string.loggedIn), Toast.LENGTH_SHORT).show()
+        }
+        else {
+            Toast.makeText(this, getString(R.string.signed_up), Toast.LENGTH_SHORT).show()
+        }
+    }
+
+}
